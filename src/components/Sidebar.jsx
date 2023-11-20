11cbbf42e63609249a619dbb2e60b2488e1a8306
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2023-11-20 15:22:42 +0600

    Iqtest design

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { MdOutlineStickyNote2 } from "react-icons/md";
+import { RxDashboard } from "react-icons/rx";
+import { RiLogoutCircleRLine } from "react-icons/ri";
+import { useSelector } from "react-redux";
+
+const Sidebar = () => {
+    let show = useSelector((state) => state.counter.value);
+    return (
+        <aside className="flex ">
+            {show ? (
+                <div className="xl:w-[15%] md:w-[25%] smalldevice:max-xl:fixed sm:max-md:w-[30%] smalldevice:w-1/2 pt-20 px-2 shadow-xl h-[100vh] fixed  z-50 bg-[#162655] ">
+                    <div className="h-[90%] ">
+                        <Link
+                            to="deshboard"
+                            className="flex items-center gap-x-2 mb-4"
+                        >
+                            <MdOutlineStickyNote2 className="text-3xl font-medium text-white" />
+                            <p className=" font-rb font-medium text-lg text-white">
+                                Dashboard
+                            </p>
+                        </Link>
+                        <Link to="iqtest" className="flex items-center gap-x-2">
+                            <RxDashboard className="  font-medium text-3xl text-white" />
+                            <p className=" font-rb font-medium text-lg text-white">
+                                Test
+                            </p>
+                        </Link>
+                    </div>
+                    <div className="h-[10%]">
+                        <Link to="/" className="flex items-center gap-x-2">
+                            <RiLogoutCircleRLine className="text-3xl font-medium text-white" />
+                            <p className=" font-rb font-medium text-lg text-white">
+                                Log out
+                            </p>
+                        </Link>
+                    </div>
+                </div>
+            ) : (
+                <div className="xl:w-[4%] bg-[#162655] fixed z-50 pt-20 smalldevice:max-xl:hidden px-2 shadow-xl h-[100vh] ">
+                    <div className="h-[90%]">
+                        <Link
+                            to="deshboard"
+                            className="flex items-center gap-x-2 mb-4"
+                        >
+                            <MdOutlineStickyNote2 className="text-3xl font-medium text-white" />
+                        </Link>
+                        <Link to="iqtest" className="flex items-center gap-x-2">
+                            <RxDashboard className="  font-medium text-3xl text-white" />
+                        </Link>
+                    </div>
+                    <div className="h-[10%]">
+                        <RiLogoutCircleRLine className="text-3xl font-medium text-white" />
+                    </div>
+                </div>
+            )}
+        </aside>
+    );
+};
+
+export default Sidebar;
--- src/components/pages/IqTest.jsx
+++ src/components/pages/IqTest.jsx
@@ -1,7 +1,65 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import Module from "../layout/Module";
 
-const IqTest = () => {
-  return <div className="h-[500px] bg-red-400">IqTest</div>;
+const IqTest = (props) => {
+    let show = useSelector((state) => state.counter.value);
+    return (
+        <section
+            className={`mt-16 flex gap-x-4 flex-wrap gap-y-4 p-4 ${
+                show ? "xl:ml-[200px]" : "xl:ml-[50px]"
+            }`}
+        >
+            <Module
+                updatetime="Updated by 1day ago"
+                modulename="Module 1"
+                moduletext="Lorem ipsum dolor sit amet consectetur. Feugiat consectetur
+                lectus sagittis tincidunt."
+                questionnumber="100"
+                durationtime="1"
+            />
+            <Module
+                updatetime="Updated by 1day ago"
+                modulename="Module 2"
+                moduletext="Lorem ipsum dolor sit amet consectetur. Feugiat consectetur
+                lectus sagittis tincidunt."
+                questionnumber="100"
+                durationtime="1"
+            />
+            <Module
+                updatetime="Updated by 1day ago"
+                modulename="Module 3"
+                moduletext="Lorem ipsum dolor sit amet consectetur. Feugiat consectetur
+                lectus sagittis tincidunt."
+                questionnumber="100"
+                durationtime="1"
+            />
+            <Module
+                updatetime="Updated by 1day ago"
+                modulename="Module 4"
+                moduletext="Lorem ipsum dolor sit amet consectetur. Feugiat consectetur
+                lectus sagittis tincidunt."
+                questionnumber="100"
+                durationtime="1"
+            />
+            <Module
+                updatetime="Updated by 1day ago"
+                modulename="Module 5"
+                moduletext="Lorem ipsum dolor sit amet consectetur. Feugiat consectetur
+                lectus sagittis tincidunt."
+                questionnumber="100"
+                durationtime="1"
+            />
+            <Module
+                updatetime="Updated by 1day ago"
+                modulename="Module 6"
+                moduletext="Lorem ipsum dolor sit amet consectetur. Feugiat consectetur
+                lectus sagittis tincidunt."
+                questionnumber="100"
+                durationtime="1"
+            />
+        </section>
+    );
 };
 
 export default IqTest;

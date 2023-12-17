286e47ed0aab278eb6e2e0024855fcf631c3e4f0
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2023-12-18 02:46:31 +0600

    design problem fix

--- src/components/pages/IqTest.jsx
+++ src/components/pages/IqTest.jsx
@@ -25,7 +25,6 @@ const IqTest = (props) => {
     const [modalIsOpen, setIsOpen] = useState(false);
     const [models, setModels] = useState([]);
     const [modelsId, setModelsId] = useState("");
-    const [modelqusstatus, setModelQusStatus] = useState("");
     let userToken = useSelector((state) => state.tokened.Token);
 
     useEffect(() => {
@@ -75,7 +74,6 @@ const IqTest = (props) => {
             );
 
             const responseData = await response.json();
-            setModelQusStatus(responseData.status);
         } catch (error) {
             console.error("Login error:", error);
             throw error;
@@ -84,30 +82,23 @@ const IqTest = (props) => {
     };
     return (
         <>
-            <section className="mt-16 flex xl:justify-end p-4 mx-auto ">
+            <section
+                className="flex xl:justify-center mt-16 p-4 w-full  "
+            >
                 <div
-                    className={`flex xl:justify-end gap-x-4 flex-wrap gap-y-4 ${
-                        show ? "xl:w-[86.2%]" : "xl:w-[98%]"
-                    }`}
+                    className={` ${
+                        show ? "xl:w-[70%]" : "xl:w-[92%]"
+                    } flex lg:max-xl:w-full gap-x-3 gap-y-3 flex-wrap`}
                 >
-                    {models.map((item, i) => (
-                        <div className="border p-5 rounded-2xl xl:w-[32%] sm:w-[48%] smalldevice:w-full shadow-md">
-                            <Module
-                                index={i}
-                                updatetime="Updated by 1day ago"
-                                modulename={item.title}
-                                moduletext={item.note}
-                                questionnumber="100"
-                                durationtime={item.exam_time}
-                                onclick={() => openModal(item)}
-                            />
-                            <button
-                                onClick={props.onclick}
-                                className="group font-rb font-bold text-lg text-center border py-[10px] w-full text-[#3888F9] rounded-lg border-[#3888F9] transition duration-300 ease-in-out hover:text-white hover:bg-[#3888F9]"
-                            >
-                                Resqust
-                            </button>
-                        </div>
+                    {models.map((item) => (
+                        <Module
+                            updatetime="Updated by 1day ago"
+                            modulename={item.title}
+                            moduletext={item.note}
+                            questionnumber="100"
+                            durationtime={item.exam_time}
+                            onclick={() => openModal(item)}
+                        />
                     ))}
                 </div>
             </section>

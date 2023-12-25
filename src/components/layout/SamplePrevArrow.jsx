1bf9a2ee1123a2aff615f795da18920e289da777
Tanvir Mahmud Tamim <tanvirmahmudtamim59@gmail.com>
2023-12-25 16:08:42 +0600

    slider added

---
+++ src/components/layout/SamplePrevArrow.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { RxTriangleLeft } from "react-icons/rx";
+const SamplePrevArrow = ({ style, onClick }) => {
+    return (
+        <span
+            className=" absolute top-[35%] left-0 z-50 !flex lg:h-16 lg:w-16 sm:max-md:h-10 sm:max-md:w-10 h-10 w-10 items-center justify-center rounded-full bg-[rgba(0,0,0,0.10)] hover:bg-[rgba(0,0,0,0.77)]"
+            style={{
+                ...style,
+            }}
+            onClick={onClick}
+        >
+            <RxTriangleLeft className="text-2xl text-white" />
+        </span>
+    );
+};
+
+export default SamplePrevArrow;
